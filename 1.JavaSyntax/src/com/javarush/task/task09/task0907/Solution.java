d5c76891a9b1598d06458e1562f765f0e971757f
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2019-11-21 21:59:50 +0300

    1

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0907/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task09.task0907;
+/* 
+Исключение при работе с числами
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        try{
+        int a = 42 / 0;
+        }
+        catch (ArithmeticException ex){
+            System.out.println(ex);
+        }
+    }
+}
