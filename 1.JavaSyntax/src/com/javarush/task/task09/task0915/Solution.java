30867232ad0c285d9aa5f37fe11c20484d7e5f32
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2019-11-30 23:28:17 +0300

    1

--- 1.JavaSyntax/src/com/javarush/task/task09/task0915/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0915/Solution.java
@@ -1,38 +1,41 @@
 package com.javarush.task.task09.task0915;
+
 import java.io.CharConversionException;
 import java.io.IOException;
 import java.nio.file.FileSystemException;
+
 /*
 Перехват выборочных исключений
 */
 public class Solution {
     public static StatelessBean BEAN = new StatelessBean();
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         try {
             handleExceptions();
-        }
-        catch (FileSystemException e){
+        } catch (FileSystemException e) {
             BEAN.log(e);
         }
     }
-    public static void handleExceptions() throws FileSystemException{
+
+    public static void handleExceptions() throws FileSystemException {
         try {
             BEAN.methodThrowExceptions();
-        }
-        catch (FileSystemException e){
+        } catch (FileSystemException e) {
             BEAN.log(e);
             throw e;
-        }catch (CharConversionException e){
+        } catch (CharConversionException e) {
             BEAN.log(e);
-        }catch (IOException e){
+        } catch (IOException e) {
             BEAN.log(e);
         }
     }
+
     public static class StatelessBean {
         public void log(Exception exception) {
             System.out.println(exception.getMessage() + ", " + exception.getClass().getSimpleName());
         }
+
         public void methodThrowExceptions() throws CharConversionException, FileSystemException, IOException {
             int i = (int) (Math.random() * 3);
             if (i == 0) {
---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0919/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task09.task0919;
+/*
+Деление на ноль
+*/
+public class Solution {
+    public static void main(String[] args) {
+        try {
+            divideByZero();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void divideByZero() {
+        System.out.println(13/0);
+
+    }
+}
