7210075fb0a90dbd8d3f7d3a2dfa0009510a781d
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2020-01-12 21:30:17 +0300

    12 января

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0930/Solution.java
@@ -0,0 +1,70 @@
+package com.javarush.task.task09.task0930;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+/*
+Задача по алгоритмам
+*/
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String s = reader.readLine();
+            if (s.isEmpty()) {
+                break;
+            }
+            list.add(s);
+        }
+
+        String[] array = list.toArray(new String[list.size()]);
+        sort(array);
+
+        for (String x : array){
+            System.out.println(x);
+        }
+    }
+
+    public static void sort(String[] array) {
+        // напишите тут ваш код
+        for (int barrier = 0; barrier < array.length; barrier++) {
+            int index = barrier;
+            String value = array[barrier];
+            for (int i = 1 + barrier; i < array.length; i++) {
+                if ((isNumber(value) && isNumber(array[i])) && (Integer.parseInt(array[i]) > Integer.parseInt(value))
+                        || (!(isNumber(value) || isNumber(array[i]))) && (isGreaterThan(value, array[i]))) {
+
+                    index = i;
+                    value = array[i];
+                }
+            }
+            array[index] = array[barrier];
+            array[barrier] = value;
+        }
+    }
+
+    // Метод для сравнения строк: 'а' больше чем 'b'
+    public static boolean isGreaterThan(String a, String b) {
+        return a.compareTo(b) > 0;
+    }
+
+
+    // Переданная строка - это число?
+    public static boolean isNumber(String s) {
+        if (s.length() == 0) {
+            return false;
+        }
+
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if ((i != 0 && c == '-') // Строка содержит '-'
+                    || (!Character.isDigit(c) && c != '-') // или не цифра и не начинается с '-'
+                    || (chars.length == 1 && c == '-')) // или одиночный '-'
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
