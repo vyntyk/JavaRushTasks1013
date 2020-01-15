bc40555391db9e23e243d6504c78f2b244577833
Viktor Belous <56500587+vyntyk@users.noreply.github.com>
2020-01-15 20:46:36 +0300

    15 января

---
+++ 2.JavaCore/src/com/javarush/task/task11/task1110/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task11.task1110;
+
+/*
+Не забываем инкапсулировать
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Cat {
+
+        public Cat(String name, int age, int weight) {
+        }
+
+        private String name;
+        private int age;
+        private int weight;
+        private int speed;
+
+        public String getName() {
+            return null;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setWeight(int weight) {
+        }
+
+        public void setSpeed(int speed) {
+        }
+    }
+}
